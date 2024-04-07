6838ba23373f58feccf6dd92e983a1d925f81b52
ankit <ankit.dhawan@employwise.com>
2024-04-07 21:01:54 +0530

    [ADDED] transport ui

--- src/screens/trip/itineraryscreen.js
+++ src/screens/trip/itineraryscreen.js
@@ -1,10 +1,55 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { View, Text, FlatList, ScrollView, TouchableOpacity, TextInput, Button } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import { Checkbox } from 'react-native-paper';
+import RBSheet from 'react-native-raw-bottom-sheet';
 
-const Itinerary = () => {
+//FUNCTIONS
+import { getTodoDetails, editTodoDetails, deleteTodoDetails, createTodoDetails } from '../../redux/trips/tripActions';
+import { tripSlice } from '../../redux/trips/tripSlice';
+
+//COMPONENTS
+import Header from '../../components/header';
+import Loading from '../../components/loading';
+import AppButton from '../../components/button';
+import AppTextInput from '../../components/textinput';
+
+//CONSTANTS
+const FILE_NAME = 'itineraryscreen.js'
+
+const Itinerary = ({navigation}) => {
+    const { tripId } = navigation.state.params;
+    const state = useSelector((state) => state.trip);
+    const dispatch = useDispatch();
+    const { resetError, resetObjectState } = tripSlice.actions;
+
+
+    const bottomSheetRef = useRef();
+    const [loading, setLoading] = useState(true);
+    const [itineraryDetails, setItineraryDetails] = useState([]);
+    const [newItinerary, setNewItinerary] = useState('');
+
+    useEffect(() => {
+        console.log(`In ${FILE_NAME}: useEffect`);
+
+        dispatch(resetObjectState('itineraryDetails'));
+        dispatch(resetError());
+    }, []);
+
+    // useEffect(() => {
+    //     if (state.todoDetails.length !== 0) {
+    //         setItineraryDetails(state.itineraryDetails[0].list);
+    //         setLoading(false);
+    //     } else {
+    //         dispatch(getItineraryDetails({ tripId }));
+    //     }
+    // }, [state.todoDetails]);
+
+    if(loading) return <Loading />
+
     return(
         <>
-            <Text>Itinerary</Text>
+            <Header navigation={navigation} title={'Itinerary'} />
         </>
     )
 };
--- src/screens/trip/transportscreen.js
+++ src/screens/trip/transportscreen.js
@@ -1,16 +1,136 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
 
 //COMPONENTS
 import Header from '../../components/header';
 
-const Transport = ({navigation}) => {
-    return(
+//ICONS
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import FontAwesom6 from 'react-native-vector-icons/FontAwesome6'
+
+//CONSTANTS
+const FILE_NAME = 'transportscreen.js';
+import { BACKGROUND, PRIMARY_BRAND_2, PRIMARY_BRAND_2_OVERLAY, TEXT, TEXTINPUT_FOCUSED, TEXT_WHITE } from '../../utils/colors';
+
+const Transport = ({ navigation }) => {
+    const navigationItems = [
+        { id: '1', title: 'Flights', icon: <MaterialIcons name={'flight'} size={18} color={TEXT_WHITE} /> },
+        { id: '2', title: 'Trains', icon: <MaterialCommunityIcons name={'train'} size={18} color={TEXT_WHITE} /> },
+        { id: '3', title: 'Bus', icon: <MaterialCommunityIcons name={'train-car-passenger'} size={18} color={TEXT_WHITE} /> },
+        { id: '4', title: 'Rentals', icon: <MaterialCommunityIcons name={'car'} size={18} color={TEXT_WHITE} /> },
+        { id: '5', title: 'Ship', icon: <FontAwesom6 name={'sailboat'} size={18} color={TEXT_WHITE} /> }
+    ];
+
+    const [selectedTab, setSelectedTab] = useState(navigationItems[0].id);
+
+    const renderTab = ({ item }) => (
+        <TouchableOpacity
+            style={[styles.tab, selectedTab === item.id && styles.selectedTab]}
+            onPress={() => setSelectedTab(item.id)}
+        >
+            {item.icon}
+            <Text style={[styles.tabText, selectedTab === item.id && {color: TEXT_WHITE}]}>{item.title}</Text>
+        </TouchableOpacity>
+    );
+
+    const renderComponent = () => {
+        switch (selectedTab) {
+            case '1':
+                return <FlightsComponent />;
+            case '2':
+                return <TrainsComponent />;
+            case '3':
+                return <BusComponent />;
+            case '4':
+                return <RentalsComponent />;
+            case '5':
+                return <ShipComponent />;
+            default:
+                return null;
+        }
+    };
+
+    // Your component for Flights
+    const FlightsComponent = () => (
+        <View style={styles.componentContainer}>
+            <Text>Flight</Text>
+        </View>
+    );
+
+    // Your component for Trains
+    const TrainsComponent = () => (
+        <View style={styles.componentContainer}>
+            <Text>Trains</Text>
+        </View>
+    );
+
+    // Your component for Bus
+    const BusComponent = () => (
+        <View style={styles.componentContainer}>
+            <Text>Bus</Text>
+        </View>
+    );
+
+    // Your component for Rentals
+    const RentalsComponent = () => (
+        <View style={styles.componentContainer}>
+            <Text>Rentals</Text>
+        </View>
+    );
+
+    // Your component for Ship
+    const ShipComponent = () => (
+        <View style={styles.componentContainer}>
+            <Text>Ship</Text>
+        </View>
+    );
+
+    return (
         <>
             <Header navigation={navigation} title={'Transport'} />
-            <Text>Transport</Text>
+
+            <View>
+                <FlatList
+                    horizontal
+                    data={navigationItems}
+                    renderItem={renderTab}
+                    keyExtractor={(item) => item.id}
+                    contentContainerStyle={styles.tabContainer}
+                    showsHorizontalScrollIndicator={false}
+                />
+                {renderComponent()}
+            </View>
         </>
-    )
+    );
 };
+
+const styles = StyleSheet.create({
+    tabContainer: {
+        paddingVertical: 10,
+        paddingHorizontal: 5,
+        borderBottomWidth: 1,
+        borderBottomColor: 'lightgray',
+    },
+    tab: {
+        marginRight: 10,
+        padding: 8,
+        borderRadius: 8,
+        backgroundColor: PRIMARY_BRAND_2_OVERLAY,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    selectedTab: {
+        backgroundColor: PRIMARY_BRAND_2
+    },
+    tabText: {
+        fontSize: 14,
+        color: TEXT,
+        marginLeft: 5
+    },
+    componentContainer: {
+        // flex: 1,
+    },
+});
 
 export default Transport;
